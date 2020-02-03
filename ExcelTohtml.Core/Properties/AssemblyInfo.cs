7a5440e5ae35343409c37e1ee4b48f736c34b9c5
Zdenek Dejmek <zdenek.dejmek@landisgyr.com>
2020-02-03 12:57:30 +0100

    GitInfo

---
+++ ExcelTohtml.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExcelToHtml")]
+[assembly: AssemblyDescription("Convert Excel to HTML - Support for .xlsx format (Microsoft Office 2007+)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marcin Kotynia")]
+[assembly: AssemblyProduct("ExcelToHtml")]
+[assembly: AssemblyCopyright("Copyright 2019")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0948799e-e964-458a-ab91-0235a1234cd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.Commits + "-" +
+#if DEBUG
+    "debug/" +
+#else
+    "release/" +
+#endif
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit)]
